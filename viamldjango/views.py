60a3ce60862aefa16ff98123390d9b42217ccf65
mdkashifsidd2020 <142329771+mdkashifsidd2020@users.noreply.github.com>
2023-08-24 11:50:42 +0530

    Add files via upload

---
+++ viamldjango/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render,HttpResponse,redirect
+from vimu.models import contact1
+import time
+
+def index(request):
+
+    if request.method=="POST":
+        fname=request.POST['fname']
+        lname=request.POST['lname']
+        email=request.POST['email']
+        phone=request.POST['no']
+        subject=request.POST['sub']
+        message=request.POST['msg']
+
+        my_form=contact1(first_name=fname,last_name=lname,email_id=email,phone_No=phone,Subject=subject,Message=message)
+        my_form.save()
+        data={
+            'msg':'thank for contacting us'}
+
+        return render(request,'index.html',data)
+        
+
+        
+    return render(request,'index.html')
+# Create your views here.
